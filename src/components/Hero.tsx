ed3e56bd560fa3c7a17bac6fcee5975bcaab0c37
samuelmemmi <samuelmemmi29@gmail.com>
2025-07-29 13:15:59 +0300

    update hero picture

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,6 +1,6 @@
 import { Button } from "@/components/ui/button";
 import { Phone, MapPin, Star } from "lucide-react";
-import heroImage from "@/assets/hero-car-wash.jpg";
+import heroImage from "@/assets/dsc03094.jpg";
 import { useLanguage } from "@/contexts/LanguageContext";
 
 const Hero = () => {
