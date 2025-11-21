c6244f2b2a1b512df6452d00b2e3f9c95a761d48
samuelmemmi <samuelmemmi29@gmail.com>
2025-11-21 14:21:59 +0200

    16

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -22,7 +22,7 @@ const Header = () => {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <div className="flex-shrink-0">
-            <img src={logo} alt="Logo" className="max-h-20" />
+            <img src={logo} alt="Logo" className="max-h-16" />
           </div>
 
 
