4c1b7e0628a38f3b55fee2ccd795493a0b9eba6b
samuelmemmi <samuelmemmi29@gmail.com>
2025-11-21 13:31:46 +0200

    lime

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -60,7 +60,7 @@ const Hero = () => {
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <a href="#booking">
               <Button 
-                variant="premium-green-new" 
+                variant="lime" 
                 size="lg" 
                 className="text-lg px-8 py-6 font-semibold"
               >
